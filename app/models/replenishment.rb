68722b433ffcdd58b8ed4297ad66d250cc866d3c
Justin Pong <jp.waves51@gmail.com>
2024-05-11 16:30:59 -0400

    rework fixtures and add scope for unfulfilled replenishments

--- app/models/replenishment.rb
+++ app/models/replenishment.rb
@@ -1,4 +1,8 @@
 class Replenishment < ApplicationRecord
   belongs_to :account
   belongs_to :product
+
+  scope :unfulfilled, -> { where(fulfilled_at: nil) }
+
+  # TODO: figure out how to add a unique index on account_id and product_id and fulfilled_at when fulfilled_at is nil, and also look into what indexes are to begin with
 end
--- test/models/replenishment_test.rb
+++ test/models/replenishment_test.rb
@@ -1,7 +1,15 @@
-require "test_helper"
+require 'test_helper'
 
 class ReplenishmentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @unfulfilled_replenishment = replenishments(:unfulfilled)
+    @fulfilled_replenishment = replenishments(:fulfilled)
+  end
+
+  test 'unfulfilled scope should return only unfulfilled replenishments' do
+    unfulfilled_replenishments = Replenishment.unfulfilled
+
+    assert_includes unfulfilled_replenishments, @unfulfilled_replenishment
+    refute_includes unfulfilled_replenishments, @fulfilled_replenishment
+  end
 end
